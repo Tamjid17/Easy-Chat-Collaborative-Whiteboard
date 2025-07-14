66c75111cb971554ade9ad7ae6bff873fd38447c
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-15 04:08:50 +0600

    refactor server setup to use environment variables and add database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express, { Express, Request, Response } from "express";
+import { connectToDB } from './database/db'
 
 const app: Express = express();
+connectToDB();
+
+const PORT = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World")
 })
 
-app.listen(3001, () => {
-    console.log(`Server is running on port 3001`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
 
