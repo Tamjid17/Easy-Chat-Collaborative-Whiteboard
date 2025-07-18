68aa4749d19fedc35dd87d2ccc3a11d7c3c26442
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-19 02:23:19 +0600

    feat: enhance user routes with block, unblock, and search functionality

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { authMiddleware } from '../middlewares/auth-middleware';
-import { createUser, loginUser, updateName, changePassword } from '../controllers/user-controller';
+import { createUser, loginUser, updateName, changePassword, blockUser, unblockUser, searchUser } from '../controllers/user-controller';
 
 const router = Router();
 
 router.post('/create', createUser);
 router.post('/login', loginUser);
-router.put('/update-name', authMiddleware, updateName);
-router.put('/change-password', authMiddleware, changePassword);
+
+router.use(authMiddleware);
+router.put('/update-name', updateName);
+router.put('/change-password', changePassword);
+router.put('/block-user/:userId', blockUser);
+router.put('/unblock-user/:userId', unblockUser);
+router.get('/search/:query', searchUser);
 
 export default router;
