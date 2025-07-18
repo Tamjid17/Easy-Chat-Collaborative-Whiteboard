c75c978a9878d35e80a4c483998400f09897ace5
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-18 21:23:38 +0600

    feat: set up user routes and JSON parsing middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Express, Request, Response } from "express";
+
 import { connectToDB } from './database/db'
+import userRoutes from './routes/user-routes';
 
 const app: Express = express();
 connectToDB();
 
 const PORT = process.env.PORT
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello World")
