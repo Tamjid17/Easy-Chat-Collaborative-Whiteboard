0d74b3dadca42660a81e0125a723d1dde679f9e2
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-19 19:41:57 +0600

    feat: add updateProfilePicture route for user profile picture updates

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from '../middlewares/auth-middleware';
-import { createUser, loginUser, updateName, changePassword, blockUser, unblockUser, searchUser } from '../controllers/user-controller';
+import { createUser, loginUser, updateName, updateProfilePicture, changePassword, blockUser, unblockUser, searchUser } from '../controllers/user-controller';
+import upload from "../middlewares/multer-middleware";
 
 const router = Router();
 
@@ -9,6 +10,7 @@ router.post('/login', loginUser);
 
 router.use(authMiddleware);
 router.put('/update-name', updateName);
+router.put('/update-picture', upload.single('profilePicture'), updateProfilePicture)
 router.put('/change-password', changePassword);
 router.put('/block-user', blockUser);
 router.put('/unblock-user', unblockUser);
