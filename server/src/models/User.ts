4f7bff8ceb9a9b67503c6407e176fb404b06e7e6
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-19 19:41:31 +0600

    feat: add profilePicturePublicId field to user schema for image management

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
+    profilePicturePublicId: {
+        type: String,
+        default: ''
+    },
     activeStatus: {
         type: Boolean,
         default: false
