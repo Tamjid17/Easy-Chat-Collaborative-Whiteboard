2f73a6e187eb505120e14a16a3d5baccaaaffcca
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-29 22:15:52 +0600

    test: check if custom colors and fonts are working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button"
 function App() {
   return (
     <div>
-      <Button className="bg-blue-500 text-white">Click Me</Button>
+      <Button className="bg-customPrimary text-white">Click Me</Button>
     </div>
   )
 }
