8c061d3055bb8fa701e867ecea3f6be39c9355cf
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-03 03:08:43 +0600

    feat: add CORS middleware for handling cross-origin requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Express, Request, Response } from "express";
+import cors from 'cors'
 
 import { connectToDB } from './database/db'
 import userRoutes from './routes/user-routes';
@@ -11,6 +12,15 @@ const app: Express = express();
 connectToDB();
 
 const PORT = process.env.PORT
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.use(express.json());
 
