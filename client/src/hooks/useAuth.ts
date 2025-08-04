8f428f3d13c831973fd5fdcc7b531d85fe6f8e65
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-04 22:06:19 +0600

    feat: enhance authentication hooks with success and error toast notifications

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,5 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { jwtDecode } from 'jwt-decode';
+import { toast } from 'sonner';
+import { AxiosError } from "axios";
 
 import { registerUser, loginUser } from '../services/authService';
 import { useAuthStore } from '../store/authStore';
@@ -9,8 +11,17 @@ export const useRegisterMutation = () => {
         mutationFn: registerUser,
         onSuccess: (data) => {
             console.log("Registration successful!", data);
+            toast.success("Registration Successful!", {
+                description: "You can now log in with your new account.",
+            });
         },
         onError: (error) => {
+            const errorMessage =
+                error instanceof AxiosError && error?.response?.data?.message ||
+                "An unknown error occurred during registration.";
+            toast.error("Registration Failed", {
+                description: errorMessage,
+            });
             console.error("Registration failed:", error);
         }
     });
@@ -25,9 +36,22 @@ export const useLoginMutation = () => {
             if(data && data.success && data.accessToken) {
                 const decodedToken: { fullName: string } = jwtDecode(data.accessToken);
                 setAuth(data.accessToken, { fullName: decodedToken.fullName, email: data.user.email, id: data.user._id, profilePicture: data.user.profilePicture });
+                toast.success("Login Successful!", {
+                    description: `Welcome back, ${decodedToken.fullName}!`,
+                });
+            } else {
+                toast.error("Login Failed", {
+                    description: data.message || "Invalid credentials provided.",
+                });
             }
         },
         onError: (e) => {
+            const errorMessage =
+                (e instanceof AxiosError && e?.response?.data?.message) ||
+                "Invalid credentials or server error.";
+            toast.error("Login Failed", {
+                description: errorMessage,
+            });
             console.error("Login failed:", e);
         }
     })
