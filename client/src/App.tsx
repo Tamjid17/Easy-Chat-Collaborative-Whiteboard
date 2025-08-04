b464b38047aadfe0eafb313f960780ac4a9f223a
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-04 22:05:56 +0600

    feat: add Toaster component for notifications in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 //import { Button } from "./components/ui/button"
 import AuthPage from "./features/AuthPage"
+import { Toaster } from "./components/ui/sonner"
 
 function App() {
   return (
     <div>
       <AuthPage />
+      <Toaster richColors position="top-center" />
     </div>
-  )
+  );
 }
 
 export default App
