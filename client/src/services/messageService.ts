e6cee893748e2d20e388a8a64abe02a5a2383fb7
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-07 23:50:05 +0600

    fix: add parameters for sendMessage sevice inside a payload obect

--- client/src/services/messageService.ts
+++ client/src/services/messageService.ts
@@ -1,6 +1,7 @@
 import apiClient from "../api/apiClient";
 
-export const sendMessage = async (conversationId: string, content: string) => {
+export const sendMessage = async (payload: {conversationId: string, content: string}) => {
+    const { conversationId, content } = payload;
     const { data } = await apiClient.post("/messages", {
         conversationId,
         content,
