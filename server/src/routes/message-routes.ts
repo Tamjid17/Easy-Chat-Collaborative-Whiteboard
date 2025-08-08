eb2dfcb0f2f5acaf6a75590fbc5be527dc2ae2dd
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-08 22:52:39 +0600

    fix: add multer middleware for file handling when sending message

--- server/src/routes/message-routes.ts
+++ server/src/routes/message-routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 import { authMiddleware } from '../middlewares/auth-middleware';
+import upload from "../middlewares/multer-middleware";
 import { sendMessage } from "../controllers/message-controller";
 
 const router = Router();
 
 router.use(authMiddleware);
 
-router.post("/", sendMessage);
-
+router.post("/", upload.single("image"), sendMessage);
 
 export default router;
