172707765312c085596a20d820a53ded0506f0b2
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-08 20:29:45 +0600

    feat: implement dynamic message rendering in ChatMessages component

--- client/src/features/chat/components/ChatMessages.tsx
+++ client/src/features/chat/components/ChatMessages.tsx
@@ -1,29 +1,26 @@
 
+import { useGetChatHistory } from "@/hooks/useConversations";
+import { useChatStore } from "@/store/chatStore";
+import { useAuthStore } from "@/store/authStore";
+import type { Message } from "@/lib/types/message";
+
 const ChatMessages = () => {
-    return (
-        <div className="flex-1 overflow-y-auto p-6 space-y-4">
-            <div className="flex justify-start">
-            <div className="bg-muted p-3 rounded-lg max-w-xs md:max-w-md">
-                <p className="text-foreground">How are you?</p>
-            </div>
-            </div>
-            <div className="flex justify-end">
-            <div className="bg-customAccentTwo text-white p-3 rounded-lg max-w-xs md:max-w-md">
-                <p>I am fine, thanks for asking!</p>
-            </div>
-            </div>
-            <div className="flex justify-start">
-            <div className="bg-muted p-3 rounded-lg max-w-xs md:max-w-md">
-                <p>What about you?</p>
-            </div>
-            </div>
-            <div className="flex justify-end">
-            <div className="bg-customAccentTwo text-white p-3 rounded-lg max-w-xs md:max-w-md">
-                <p>Doing great!</p>
-            </div>
-            </div>
-        </div>
-    );
-}
+    const { selectedConversationId } = useChatStore();
+    const { data: chatHistory, isLoading } = useGetChatHistory(selectedConversationId);
+    const loggedInUser = useAuthStore((state) => state.user);
+     
+  return (
+    <div className="flex-1 overflow-y-auto p-6 space-y-4">
+      {isLoading && <p>Loading messages...</p>}
+      {chatHistory?.conversation?.messages.map((message: Message) => (
+                <div key={message._id} className={`flex ${message.sender._id === loggedInUser?._id ? 'justify-end' : 'justify-start'}`}>
+                    <div className= {`max-w-xs p-3 rounded-lg ${message.sender._id === loggedInUser?._id ? 'bg-customAccentTwo text-white' : 'bg-muted text-foreground'}`}>
+                        <p>{message.content}</p>
+                    </div>
+                </div>
+            ))}
+    </div>
+  );
+};
 
 export default ChatMessages;
