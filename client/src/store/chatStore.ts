6722803191a5f560de6257d86b846d09ad20bd98
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-09 04:36:55 +0600

    feat: add online user management to chat store

--- client/src/store/chatStore.ts
+++ client/src/store/chatStore.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface ChatState {
   selectedConversationId: string | null;
   setSelectedConversationId: (id: string | null) => void;
+  onlineUserIds: string[];
+  setOnlineUserIds: (ids: string[]) => void;
 }
 
 export const useChatStore = create<ChatState>((set) => ({
   selectedConversationId: null,
   setSelectedConversationId: (id) => set({ selectedConversationId: id }),
+  onlineUserIds: [],
+  setOnlineUserIds: (ids) => set({ onlineUserIds: ids }),
 }));
