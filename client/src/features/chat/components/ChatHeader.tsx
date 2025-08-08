e608799756585ea06a133765efa5589622514bbe
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-08 20:34:57 +0600

    feat: update ChatHeader to display other user's profile and loading state

--- client/src/features/chat/components/ChatHeader.tsx
+++ client/src/features/chat/components/ChatHeader.tsx
@@ -1,17 +1,49 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+import { useChatStore } from "@/store/chatStore";
+import { useGetChatHistory } from "@/hooks/useConversations";
+import { useAuthStore } from "@/store/authStore";
+import { Skeleton } from "@/components/ui/skeleton";
+
+
 const ChatHeader = () => {
+    const { selectedConversationId } = useChatStore();
+    const loggedInUser = useAuthStore((state) => state.user);
+
+    const { data: conversationData, isLoading } = useGetChatHistory(selectedConversationId);
+
+    if (isLoading) {
+      return (
+        <header className="flex items-center gap-4 p-4 border-b border-border/40">
+          <Skeleton className="h-10 w-10 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[150px]" />
+            <Skeleton className="h-4 w-[100px]" />
+          </div>
+        </header>
+      );
+    }
+
+    const otherUser = conversationData?.conversation.participants.find((p: any) => p._id !== loggedInUser?._id);
+
+    if (!otherUser) {
+        return (
+        <header className="flex items-center gap-4 p-4 border-b border-border/40">
+        User not found
+        </header>
+        );
+    }
+
     return (
         <header className="flex items-center gap-4 p-4 border-b border-border/40">
         <Avatar>
             <AvatarImage
-            src="https://placehold.co/40x40/F8FFE5/4E4187?text=J"
-            alt="John Doe"
+            src={otherUser.profilePicture} alt={otherUser.fullName}
             />
-            <AvatarFallback>JD</AvatarFallback>
+            <AvatarFallback>{otherUser.fullName.slice(0, 2).toUpperCase()}</AvatarFallback>
         </Avatar>
         <div>
-            <h2 className="text-lg font-bold text-customPrimary">John Doe</h2>
+            <h2 className="text-lg font-bold text-customPrimary">{otherUser.fullName}</h2>
             <div className="flex items-center gap-2">
             <span className="h-2 w-2 rounded-full bg-customAccentOne"></span>
             <p className="text-sm text-muted-foreground">Online</p>
