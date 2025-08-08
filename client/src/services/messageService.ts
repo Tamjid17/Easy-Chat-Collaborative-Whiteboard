66eaf52ac653de31e4d695ed1e6f3f264431f3ba
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-08 22:52:56 +0600

    feat: enhance sendMessage function to support file uploads

--- client/src/services/messageService.ts
+++ client/src/services/messageService.ts
@@ -1,10 +1,32 @@
 import apiClient from "../api/apiClient";
 
-export const sendMessage = async (payload: {conversationId: string, content: string}) => {
-    const { conversationId, content } = payload;
-    const { data } = await apiClient.post("/messages", {
-        conversationId,
-        content,
-    });
-    return data;
+interface SendMessagePayload {
+    conversationId: string;
+    content?: string;
+    file?: File | null;
+}
+
+export const sendMessage = async (payload: SendMessagePayload) => {
+    if(payload.file) {
+        const formData = new FormData();
+        formData.append("conversationId", payload.conversationId);
+        if(payload.content) {
+            formData.append("content", payload.content);
+        }
+        formData.append("image", payload.file);
+
+        const { data } = await apiClient.post("/messages", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return data.message;
+    } else {
+        const { conversationId, content } = payload;
+        const { data } = await apiClient.post("/messages", {
+            conversationId,
+            content,
+        });
+        return data.message;
+    }
 };
