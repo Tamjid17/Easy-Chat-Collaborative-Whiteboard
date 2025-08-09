f6d063a86f8d3e5d56a7234bbc6c9ffab47c43a1
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-10 04:11:23 +0600

    refactor: replace socket state management with useRef for improved performance

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 import { useAuthStore } from "@/store/authStore";
 import { SocketContext } from "@/hooks/useSocket";
+import { useChatStore } from "@/store/chatStore";
 
 export const SocketContextProvider = ({
     children,
     }: {
     children: React.ReactNode;
     }) => {
-    const [socket, setSocket] = useState<Socket | null>(null);
+    const socketRef = useRef<Socket | null>(null);
     const { user, isAuthenticated } = useAuthStore();
+    const { setOnlineUserIds } = useChatStore();
 
     useEffect(() => {
-        if (isAuthenticated && user) {
-        const newSocket = io("http://localhost:3001", {
-            query: {
-            userId: user._id,
-            },
-        });
+        if (isAuthenticated && user && !socketRef.current) {
+            const newSocket = io("http://localhost:3001", {
+                query: {
+                userId: user._id,
+                },
+            });
 
-        setSocket(newSocket);
+            socketRef.current = newSocket;
 
-        return () => {
-            newSocket.close();
-        };
-        } else {
-        if (socket) {
-            socket.close();
-            setSocket(null);
+            newSocket.on("onlineUsers", (ids: string[]) => {
+                setOnlineUserIds(ids);
+            });
+    }
+    return () => {
+        if (socketRef.current) {
+            socketRef.current.close();
+            socketRef.current = null;
         }
-        }
-    }, [isAuthenticated, user, socket]);
+    };
+  }, [isAuthenticated, user, setOnlineUserIds]);
 
-    return (
-        <SocketContext.Provider value={{ socket }}>
-        {children}
+  return (
+    <SocketContext.Provider value={{ socket: socketRef.current }}>
+      {children}
         </SocketContext.Provider>
     );
 };
