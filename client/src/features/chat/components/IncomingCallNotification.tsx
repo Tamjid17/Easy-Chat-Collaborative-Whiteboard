68d28d3d2943dbb952ca972b7053c8950889fc10
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-10 22:02:53 +0600

    feat: add rejectCall functionality to IncomingCallNotification for improved call management

--- client/src/features/chat/components/IncomingCallNotification.tsx
+++ client/src/features/chat/components/IncomingCallNotification.tsx
@@ -1,24 +1,37 @@
 import { useCall } from "@/hooks/useCall";
 import { Button } from "@/components/ui/button";
-import { Phone } from "lucide-react";
+import { Phone, PhoneOff } from "lucide-react";
 
 export const IncomingCallNotification = () => {
-  const { call, answerCall } = useCall();
+  const { call, answerCall, rejectCall } = useCall();
 
   if (!call?.isReceivingCall) return null;
 
   return (
-    <div className="fixed top-5 right-5 bg-card p-4 rounded-lg shadow-lg flex items-center gap-4 z-50">
-      <p>
+    <div className="fixed top-5 right-5 bg-card p-4 rounded-lg shadow-lg border z-50 min-w-[280px]">
+      <div className="flex flex-col gap-3">
+      <p className="text-center">
         <span className="font-bold">{call.name}</span> is calling...
       </p>
+      <div className="flex justify-center gap-3">
       <Button
         size="icon"
         className="bg-customAccentOne hover:bg-customAccentOne/90"
         onClick={answerCall}
-      >
+        >
         <Phone className="h-5 w-5" />
       </Button>
+
+      <Button
+        size="icon"
+        variant="destructive"
+        className="flex item-center gap-2"
+        onClick={rejectCall}
+      >
+        <PhoneOff className="h-5 w-5" />
+      </Button>
+      </div>
+      </div>
     </div>
   );
 };
